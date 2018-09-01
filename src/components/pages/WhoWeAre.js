fa951cde016ca5c1ead581828fafdde6ca4eb759
Bratalix <bratalix@gmail.com>
2018-09-01 13:20:33 +1000

    Typo and facebook added

--- src/components/pages/WhoWeAre.js
+++ src/components/pages/WhoWeAre.js
@@ -34,71 +34,71 @@ class WhoWeAre extends Component {
                       <div className="node node-page clearfix">
                         <div className="content">
 
-                        <h2>We are disciples of Jesus</h2>
+                          <h2>We are disciples of Jesus</h2>
 
-                        <p>esus Christ made some seriously big claims. He said he was God’s Son come to Earth.
-                        He backed up his claims by performing miracles and living a perfect life.
-                        He loved God with all his heart soul and mind, and he cared for every person that he encountered.
-                        He lived the life that you and I should have lived. He had a perfect record.
-                        But instead of claiming the reward all for himself, he died on a cross. Why? Grace. On the cross Jesus takes our bad record and gives us his perfect record.
+                          <p>Jesus Christ made some seriously big claims. He said he was God’s Son come to Earth.
+                          He backed up his claims by performing miracles and living a perfect life.
+                          He loved God with all his heart soul and mind, and he cared for every person that he encountered.
+                          He lived the life that you and I should have lived. He had a perfect record.
+                          But instead of claiming the reward all for himself, he died on a cross. Why? Grace. On the cross Jesus takes our bad record and gives us his perfect record.
                         And then he rose again from the dead to prove it’s all true. This is the good news as written in the Bible, and this is why we follow Jesus</p>
 
-                        <p>
-                          So a disciple is someone who lives in response to this good news. They live a cross shaped life; no longer living for themselves but for Jesus.
-                          A disciple places Jesus agenda above their own agenda, and the needs of others above their own needs.
-                          This is a costly life, but Jesus promises not to leave us on our own. He gives us the Holy Spirit and a community of people to serve with.
+                          <p>
+                            So a disciple is someone who lives in response to this good news. They live a cross shaped life; no longer living for themselves but for Jesus.
+                            A disciple places Jesus agenda above their own agenda, and the needs of others above their own needs.
+                            This is a costly life, but Jesus promises not to leave us on our own. He gives us the Holy Spirit and a community of people to serve with.
                         </p>
 
-                        <h2>We are on mission</h2>
+                          <h2>We are on mission</h2>
 
-                        <p>
-                          Jesus told his followers to go into all of the world and make disciples. He told them to do it ‘as they go’.
-                          This means as we live our lives for Jesus, we encourage others to do the same.
-                          As we’re at home, at work, at school or uni, playing sport or whatever we do, we make disciples of Jesus as we go.
-                          Jesus died on a cross, rose from the dead, and told us he is coming back to earth anytime soon.
-                          So the best use of our short time is to live for him, and teach others to do the same.
+                          <p>
+                            Jesus told his followers to go into all of the world and make disciples. He told them to do it ‘as they go’.
+                            This means as we live our lives for Jesus, we encourage others to do the same.
+                            As we’re at home, at work, at school or uni, playing sport or whatever we do, we make disciples of Jesus as we go.
+                            Jesus died on a cross, rose from the dead, and told us he is coming back to earth anytime soon.
+                            So the best use of our short time is to live for him, and teach others to do the same.
                         </p>
 
-                        <h2>We are Evangelical and Reformed</h2>
+                          <h2>We are Evangelical and Reformed</h2>
 
-                        <p>
-                          Evangelical comes from the greek word evangelion, meaning gospel or good news. The gospel is God’s powerful message to save the world.
-                          And its for all people, in all places, at all times. It’s always relevant because it answers our deepest questions about life, and
-                          fulfils our deepest longings. As Evangelicals the gospel message is at the front and centre of all we do.
+                          <p>
+                            Evangelical comes from the greek word evangelion, meaning gospel or good news. The gospel is God’s powerful message to save the world.
+                            And its for all people, in all places, at all times. It’s always relevant because it answers our deepest questions about life, and
+                            fulfils our deepest longings. As Evangelicals the gospel message is at the front and centre of all we do.
                         </p>
 
-                        <p>BPC is apart of the Presbyterian Church of Australia.
+                          <p>BPC is apart of the Presbyterian Church of Australia.
                         What we believe about God and worship is more fully outlined in the Westminster Confession. (attach link to confession).</p>
 
-                        <h2>On Sundays</h2>
+                          <h2>On Sundays</h2>
 
-                        <p>
-                          On Sundays we want the gospel message to shape all we do. Our preaching, music, and prayers aim to do this in a way that connects with our local community.
+                          <p>
+                            On Sundays we want the gospel message to shape all we do. Our preaching, music, and prayers aim to do this in a way that connects with our local community.
                         </p>
 
-                        <p>
-                          At BPC, we believe children are valued members of the covenant community.We encourage families (parents and children) to worship together.We don’t have a Sunday School.
-                          We believe worshiping with our children is both biblical and beneficial. You can read more about that here (attach link to doc)
+                          <p>
+                            At BPC, we believe children are valued members of the covenant community.We encourage families (parents and children) to worship together.We don’t have a Sunday School.
+                            We believe worshiping with our children is both biblical and beneficial. You can read more about that here (attach link to doc)
                         </p>
 
                         </div>
-
-
-
-                </div>
-                  </div>
-                </div>  </div>
-
-
-
-                        </div>
-
 
 
 
                       </div>
                     </div>
-                  </div>
+                  </div>  </div>
+
+
+
+              </div>
+
+
+
+
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
