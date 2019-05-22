26f18682292b797d5937c8d343e202d742c4fed2
Bratalix <bratalix@gmail.com>
2019-05-22 13:40:26 +1000

    Updating phone number

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -156,7 +156,7 @@ class ContactUs extends Component {
                           <h5>Contact Us</h5>
                           <p><b>Facebook:</b> &nbsp;<a href="http://facebook.com/bannockburnpressiechurch">/bannockburnpressiechurch</a><br />
                             <b>Email:</b>&nbsp;<a href="mailto:info@bannopc.org">info@bannopc.org</a><br />
-                            <b>Phone:</b>&nbsp; 03 5281 2437</p>
+                            <b>Phone:</b>&nbsp; 0406 588 120</p>
                         </div>
 
                       </div>
